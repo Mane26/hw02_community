3e75a03726846442f9c06b9694f505cbe04f28ec
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2022-08-11 18:21:53 +0300

    изменения после ревью

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
